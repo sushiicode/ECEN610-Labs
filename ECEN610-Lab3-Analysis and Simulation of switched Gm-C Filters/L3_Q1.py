0ccda41a648da45cf6bf8c9925ab97613bfebb75
Sushanth Krishna Mithran <79506304+sushiicode@users.noreply.github.com>
2025-03-28 16:55:14 -0500

    Update L3_Q1.py

--- ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q1.py
+++ ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q1.py
@@ -12,7 +12,7 @@ num = fact * np.array([1, 1, 1, 1, 1, 1, 1, 1])
 denom = fact * np.array([1])
 freq, tf = freqz(num, denom)
 
-plt.plot(freq, 20 * np.log10(abs(tf)), color='green')  # Set plot color to green
+plt.plot(freq, 20 * np.log10(abs(tf)), color='green')
 plt.grid()
 plt.xlabel('Frequency in rad/sec')
 plt.ylabel('Amplitude')
@@ -21,7 +21,7 @@ plt.show()
 
 
 #Q1b
-Capacitors doesnt get discharged
+#Capacitors doesnt get discharged
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.signal import freqz
@@ -29,17 +29,13 @@ from scipy.signal import freqz
 N = 8
 Cs = 15.925e-12
 freq = 2.4e9
-fact = 1 / 2 * freq * Csnum = fact * np.array([1, 1, 1, 1, 1, 1, 1, 1])
+fact = 1 / 2 * freq * Cs
+num = fact * np.array([1, 1, 1, 1, 1, 1, 1, 1])
 denom = fact * np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1])
 freq, tf = freqz(num, denom)
-plt.plot(freq, 20 * np.log10(abs(tf)), color='green')  # Set plot color to green
+plt.plot(freq, 20 * np.log10(abs(tf)), color='green') 
 plt.grid()
 plt.xlabel('Frequency in [rad/sec]')
 plt.ylabel('Amplitude')
 plt.title('Response when Capacitors are Not Discharged')
 plt.show()
-
-
-
-
-
