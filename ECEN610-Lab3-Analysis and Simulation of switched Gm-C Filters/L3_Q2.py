1cba4ab1c1cc2d51bf2cb468bf4e97bd7144820c
Sushanth Krishna Mithran <79506304+sushiicode@users.noreply.github.com>
2025-03-28 15:46:09 -0500

    Update L3_Q2.py

--- ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q2.py
+++ ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q2.py
@@ -1,1 +1,22 @@
+#Q2a
+#Effect of adding CH capacitor
 
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import freqz
+
+N = 8
+C_History= 15.425e-12
+Cr = 0.5e-9
+freq = 2.4e9
+factor=1/2*freq*Cr
+a=C_History/C_History+Cr
+numerator=factor*np.array([1,1,1,1,1,1,1,1])
+denominator=factor*np.array([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-a])
+freq,tf=freqz(numerator,denominator)
+plt.plot(freq,20*np.log10(abs(tf)), color = 'green')
+plt.grid()
+plt.xlabel('Frequency in [rad/sec]')
+plt.ylabel('Amplitude')
+plt.title('Magnitude Response with History Capacitor')
+plt.show()
