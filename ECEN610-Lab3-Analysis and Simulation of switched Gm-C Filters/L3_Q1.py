7c92eb123198899e7c7e978b28d8c3cef9c6b23c
Sushanth Krishna Mithran <79506304+sushiicode@users.noreply.github.com>
2025-03-28 15:25:00 -0500

    Update L3_Q1.py

--- ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q1.py
+++ ECEN610-Lab3-Analysis and Simulation of switched Gm-C Filters/L3_Q1.py
@@ -1,4 +1,4 @@
-Q1. a
+#Q1.A
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -20,24 +20,35 @@ plt.title('Magnitude Response- Capacitor is Discharged')
 plt.show()
 
 
-#Q1b
-# Capacitors doesnt get discharged
+#Q1.B
 
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.signal import freqz
+# #Q1b
+# # Capacitors doesnt get discharged
+#
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from scipy.signal import freqz
+#
+# N = 8
+# Cs = 15.925e-12
+# freq = 2.4e9
+# fact = 1 / 2 * freq * Cs
+# num = fact * np.array([1, 1, 1, 1, 1, 1, 1, 1])
+# denom = fact * np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1])
+#
+# freq, tf = freqz(num, denom)
+# plt.plot(freq, 20 * np.log10(abs(tf)), color='green')  # Set plot color to green
+# plt.grid()
+# plt.xlabel('Frequency in [rad/sec]')
+# plt.ylabel('Amplitude')
+# plt.title('Response when Capacitors are Not Discharged')
+# plt.show()
 
-N = 8
-Cs = 15.925e-12
-freq = 2.4e9
-fact = 1 / 2 * freq * Cs
-num = fact * np.array([1, 1, 1, 1, 1, 1, 1, 1])
-denom = fact * np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1])
 
-freq, tf = freqz(num, denom)
-plt.plot(freq, 20 * np.log10(abs(tf)), color='green')  # Set plot color to green
-plt.grid()
-plt.xlabel('Frequency in [rad/sec]')
-plt.ylabel('Amplitude')
-plt.title('Response when Capacitors are Not Discharged')
-plt.show()
+
+
+
+
+
+
+
