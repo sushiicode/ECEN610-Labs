4852dc6b86fb882192e4c47285262c39653874e8
Sushanth Krishna Mithran <79506304+sushiicode@users.noreply.github.com>
2025-04-11 21:08:56 -0500

    Update Q5.py

--- ECEN610-Lab-5-Data Conversion Basics/Q5.py
+++ ECEN610-Lab-5-Data Conversion Basics/Q5.py
@@ -1,1 +1,33 @@
+#5a
+import matplotlib.pyplot as plt
+import numpy as np
+DNL = [0, -0.5, 0, 0.5, -1, 0.5, 0.5, 0]
+INL = np.cumsum(DNL)
+code = np.arange(0, 8)
+ideal = code + 0.5
+ideal = ideal + 0.5
+plt.plot(code, ideal, label='Ideal')
+plt.plot(code, INL, label='ADC', color='green')
+plt.xlabel('Code')
+plt.ylabel('Voltage (LSB)')
+plt.title('Transfer Curve')
+plt.legend()
+plt.grid(True)
+plt.show()
 
+
+#5b
+import matplotlib.pyplot as plt
+import numpy as np
+DNL = [0, -0.5, 0, 0.5, -1, 0.5, 0.5, 0]
+INL = np.cumsum(DNL)
+code = np.arange(0, 8)
+ideal = code + 0.5
+ideal = ideal + 0.5
+plt.plot(code, ideal, label='Ideal')
+plt.plot(code, INL, label='ADC')
+plt.xlabel('Code')
+plt.ylabel('Voltage (LSB)')
+plt.title('Transfer Curve')
+plt.legend()
+plt.show()
