87e9ea03bc513669401a29bc1df94d4751d437a6
Sushanth Krishna Mithran <79506304+sushiicode@users.noreply.github.com>
2025-04-11 20:17:13 -0500

    Update Q3.py

--- ECEN610-Lab-5-Data Conversion Basics/Q3.py
+++ ECEN610-Lab-5-Data Conversion Basics/Q3.py
@@ -65,3 +65,48 @@ m2.set_xticks(code)
 m2.set_ylim(-0.6, 0.2)
 plt.tight_layout()
 plt.show()
+
+Q3.D
+
+# Q3d
+import numpy as np
+import matplotlib.pyplot as plt
+
+DAC_lvl = np.array([-0.01, 0.105, 0.195, 0.28, 0.37, 0.48, 0.6, 0.75])
+ideal_lvl = np.arange(0, 0.8, 0.1)
+code = np.arange(0, 8, 1)
+
+offset_err = DAC_lvl[0] - ideal_lvl[0]
+error_fs = DAC_lvl[7] - ideal_lvl[7]
+dnl = np.zeros(8)
+inl = np.zeros(8)
+corr_lvl_dac = (DAC_lvl + 0.01) / 1.0857
+
+inl[0] = dnl[0]
+for i in range(1, 8):
+    dnl[i] = (corr_lvl_dac[i] - corr_lvl_dac[i - 1] - 0.1) / 0.1
+    inl[i] = inl[i - 1] + dnl[i]
+
+plt.figure()
+plt.subplot(2, 1, 1)
+plt.stem(code, dnl, linefmt='b-', markerfmt='bo', basefmt='k')
+plt.xlabel('Code')
+plt.ylabel('DNL')
+plt.title('Differential Nonlinearity (DNL)')
+
+plt.subplot(2, 1, 2)
+plt.stem(code, inl, linefmt='g-', markerfmt='gx', basefmt='k')
+plt.xlabel('Code')
+plt.ylabel('INL')
+plt.title('Integral Nonlinearity (INL)')
+
+plt.tight_layout()
+plt.show()
+
+print('Actual =', DAC_lvl)
+print('Ideal =', ideal_lvl)
+print('Offset Error =', offset_err)
+print('Full Scale Error =', error_fs)
+print('Corrected =', corr_lvl_dac)
+print('INL =', inl)
+print('DNL =', dnl)
